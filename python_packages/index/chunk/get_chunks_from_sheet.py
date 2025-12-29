ea654434e70007edaa18023ec33a696a6d621dd6
Pulipuli Chen <pulipuli.chen@gmail.com>
2025-12-29 09:13:46 +0800

    塞進資料庫了，然後是查詢 -Mon Dec 29 09:13:46 AM CST 2025

--- python_packages/index/chunk/get_chunks_from_sheet.py
+++ python_packages/index/chunk/get_chunks_from_sheet.py
@@ -36,7 +36,7 @@ def get_chunks_from_sheet(knowledge_id: str, section_name: str) -> list[str]:
             filepath = os.path.realpath(filepath)
 
             # logger.info(f"islink: {filepath}")
-            os.system(f"cat '{filepath}' > /dev/null"")
+            os.system(f"cat '{filepath}' > /dev/null")
             # filepath = os.path.join('/tmp', os.path.basename(filepath))
             # logger.info(f"tmp: {filepath}")
 
