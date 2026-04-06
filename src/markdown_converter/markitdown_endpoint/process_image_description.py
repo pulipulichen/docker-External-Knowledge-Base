9c3c3f523a877bf599cce119a196a69ca55ba17c
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-04-06 17:47:35 +0800

    大致上可行，現在來處理小圖不要顯示

--- src/markdown_converter/markitdown_endpoint/process_image_description.py
+++ src/markdown_converter/markitdown_endpoint/process_image_description.py
@@ -7,9 +7,7 @@ def process_image_description(markdown_content: str) -> str:
 
     def replacer(match):
         base64_data = match.group(2)
-        # length = len(base64_data)
-        # return f'PLACEHOLDER: {length}: {base64_data[0:20]}...{base64_data[-20:]}'
         description = image_describe(base64_data)
-        return f'PLACEHOLDER: {description}'
+        return f'`IMAGE: {description}`'
 
     return re.sub(pattern, replacer, markdown_content)
