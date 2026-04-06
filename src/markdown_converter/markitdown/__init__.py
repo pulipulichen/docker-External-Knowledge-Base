633039522534e4e6578436c747c5a8986dae50d8
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-04-06 15:22:08 +0800

    變更markdown converter的功能 -Mon Apr  6 03:22:08 PM CST 2026

--- src/markdown_converter/markitdown/__init__.py
+++
@@ -1,7 +0,0 @@
-from .markitdown import convert_bp, convert_file_path_to_markdown_content, get_markitdown
-
-__all__ = [
-    "convert_bp",
-    "convert_file_path_to_markdown_content",
-    "get_markitdown",
-]
