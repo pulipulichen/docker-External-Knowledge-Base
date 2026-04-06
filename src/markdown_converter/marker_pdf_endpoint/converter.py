732479ca6e6df0cb428abe75321a96f768c71174
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-04-06 16:32:55 +0800

    變更markdown converter的功能：改用marker -Mon Apr  6 04:32:55 PM CST 2026

--- src/markdown_converter/marker_pdf_endpoint/converter.py
+++ src/markdown_converter/marker_pdf_endpoint/converter.py
@@ -10,7 +10,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 config = {
-    "output_format": "markdown"
+    "output_format": "markdown",
+    "use_vllm": False,             # 如果沒有大量 GPU 顯存，關閉 vLLM
+    "disable_ocr": True,           # 如果你的 docx/pdf 文本是可選取的，禁用 OCR 速度會大幅提升
+    "force_ocr": False,
+    "paginate": False,             # 禁用分頁處理
+    "languages": "zh",             # 指定單一語言，避免模型掃描多國語言
 }
 config_parser = ConfigParser(config)
 artifact_dict = create_model_dict()
@@ -19,7 +24,7 @@ converter = PdfConverter(artifact_dict=artifact_dict)
 def convert_file_path_to_markdown_content(file_path):
     rendered = converter(file_path)
 
-    logger.info("rendered: %s", rendered)
+    # logger.info("rendered: %s", rendered)
 
     full_text, _, images = text_from_rendered(rendered)
     # 替換 Markdown 中的圖片語法
