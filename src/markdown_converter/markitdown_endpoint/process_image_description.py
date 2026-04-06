1fca126802165097bc0c74bddbb25b668b6264de
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-04-06 17:30:53 +0800

    變更markdown converter的功能：放棄marker -Mon Apr  6 05:30:53 PM CST 2026

--- src/markdown_converter/markitdown_endpoint/process_image_description.py
+++ src/markdown_converter/markitdown_endpoint/process_image_description.py
@@ -1,11 +1,15 @@
 import re
 
+from .image_describe import image_describe
+
 def process_image_description(markdown_content: str) -> str:
     pattern = r'!\[\]\(data:image\/([^;]+);base64,([^\)]+)\)'
 
     def replacer(match):
         base64_data = match.group(2)
-        length = len(base64_data)
-        return f'PLACEHOLDER: {length}: {base64_data[0:20]}...{base64_data[-20:]}'
+        # length = len(base64_data)
+        # return f'PLACEHOLDER: {length}: {base64_data[0:20]}...{base64_data[-20:]}'
+        description = image_describe(base64_data)
+        return f'PLACEHOLDER: {description}'
 
     return re.sub(pattern, replacer, markdown_content)
