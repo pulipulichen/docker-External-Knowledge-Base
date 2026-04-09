9cb6e36859a977cb1df2739bd7aa35727b6f37ec
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-04-09 12:38:13 +0800

    部分檔案沒有辦法被索引，為什麼 - Thu Apr  9 12:38:13 PM CST 2026

--- src/api/python_packages/knowledge_base_config/get_knowledge_base_config.py
+++ src/api/python_packages/knowledge_base_config/get_knowledge_base_config.py
@@ -28,7 +28,7 @@ def get_knowledge_base_config(knowledge_id):
         config_path = os.path.join(CONFIG_DIR, knowledge_id + '.yaml')
 
         if not os.path.exists(config_path):
-            logger.error(f"Knowledge base config file not found at {config_path}", stack_info=True)
+            logger.error(f"Knowledge base config file '{knowledge_id}.yml' not found at {config_path}", stack_info=True)
             return {}
 
     with open(config_path, 'r') as file:
