b766db89dfc93876856a4a30947b839c9c6a0479
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-04-16 18:36:54 +0800

    部分檔案沒有辦法被索引，為什麼 - Thu Apr 16 06:36:54 PM CST 2026

--- src/api/python_packages/search/search.py
+++ src/api/python_packages/search/search.py
@@ -130,7 +130,7 @@ def _call_searxng(
         params["time_range"] = time_range
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
     }
 
     logger.info(f"searxng endpoint: {endpoint}")
