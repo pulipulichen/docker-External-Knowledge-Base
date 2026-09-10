bf1140014d90c76a2676c7ca4321e327a30517b4
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-09-10 15:47:38 +0800

    Enhance docker-up-logs.sh: Change directory to script location before executing git and docker commands.

--- docker-up-logs.sh
+++ docker-up-logs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 git pull
 docker compose down
 docker compose up -d --build
---
+++ reload-nginx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+git pull
+
+docker-compose exec nginx nginx -t &&
+docker-compose exec nginx nginx -s reload
